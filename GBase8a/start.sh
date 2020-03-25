79d7d6ce6b98d7196d0dbc0a47fa6270697cd212
shihd <shihd@fastmail.com>
2020-03-25 09:44:22 +0800

    '1.0'

--- GBase8a/start.sh
+++ GBase8a/start.sh
@@ -3,9 +3,11 @@
 source ~/.bashrc
 
 gbase.server start
+
 sleep 3
 
 gbase -uroot -e "SET PASSWORD FOR root = PASSWORD('root')"
 
 echo `lsof -i:5258`
 
+tail -f /home/gbase/GBase/log/gbase8a/system.log
